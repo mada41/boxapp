95f5370297ba8008e3b2344fcf96da83e384f62e
Mada Aryakusumah <mada@41studio.com>
2016-03-29 11:42:47 +0700

    add API doc

--- config/application.rb
+++ config/application.rb
@@ -20,8 +20,12 @@ module Mothership
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
 
+
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
     config.active_job.queue_adapter = :sidekiq
+
+    config.paths.add File.join('app', 'api'), glob: File.join('**', '*.rb')
+    config.autoload_paths += Dir[Rails.root.join('app', 'api', '*')]
   end
 end
